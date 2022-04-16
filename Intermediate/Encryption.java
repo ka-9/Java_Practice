a81e2e471078b55661db041613b1626013675d4f
Kevin Aoun <kevin.w.aoun@gmail.com>
2022-04-16 12:10:01 +0300

    Defined main method and the supporting method encrypt

---
+++ Intermediate/Encryption.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Encryption {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputStr, encryptedStr, decryptedStr;
+        System.out.println("Enetr S: ");
+        inputStr = scan.nextLine();
+
+        encryptedStr = encrypt(inputStr);
+        decryptedStr = decrypt(encryptedStr);
+
+        System.out.println("Encrypted message: " + encryptedStr);
+        System.out.println("Decrypted message: " + decryptedStr);
+    }
+    // Exam is fun becomes 5 4 4 7 Easfnxmiu
+    private static String encrypt(String inputStr) {
+        String output;
+        output = evenCount(inputStr) + " " + oddCount(inputStr) + " " + wsIndex(inputStr) + " " + encryptEven(inputStr) + encryptOdd(inputStr);
+
+    }
+}
