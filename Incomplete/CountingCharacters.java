b4a807e880963d64da4f540ef8c02296a385251b
Kevin Aoun <kevin.w.aoun@gmail.com>
2022-05-04 13:08:06 +0300

    Fixed code, now fully working

--- Incomplete/CountingCharacters.java
+++ Incomplete/CountingCharacters.java
@@ -1,6 +1,7 @@
 package Incomplete;
 
 import java.util.Scanner;
+import java.util.Arrays;
 public class CountingCharacters {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -15,27 +16,31 @@ public class CountingCharacters {
     } 
 
     private static void checkETNOccurences (String[] stringList) {
-        int[] counters = {0, 1, 2};
         String etn = "etn";
+        int[] occurences = new int[3];
 
-        // Checks if there are no occurences at all before processing
-        for (int idx1 = 0; idx1 < etn.length(); idx1++) {
-            if (stringList[idx1].indexOf(etn.charAt(idx1)) == -1) {
-                System.out.println("No occurences of " + etn.charAt(idx1) + " in string number " + (idx1 + 1)); 
+        // Checks if there is no String at all before processing
+        for (int idx = 0; idx < stringList.length; idx++){
+            if (stringList[idx] == null || stringList[idx].length() == 0){
+                continue;
             }
-            
-            //bug counters[idx1] not working properly
-            // Checking for the number of occurences by cycling through the arrays
-            for (int idx2 = 0; idx2 < stringList[idx1].length(); idx2++) {
-                if (stringList[idx1].charAt(idx2) == etn.charAt(idx1)) {
-                    counters[idx1]++;
+            int currentIndex = 0;
+            for (int idx2 =0; idx2 < stringList[idx].length(); idx2++) {
+                currentIndex = etn.indexOf(stringList[idx].charAt(idx2));
+                switch(currentIndex) {
+                    case -1:
+                    continue;
+                    case 0:
+                    occurences[0]++;
+                    break;
+                    case 1:
+                    occurences[1]++;
+                    break;
+                    case 2:
+                    occurences[2]++;
                 }
             }
         }
-        for (int idx3 = 0; idx3 < counters.length; idx3++) {
-            if (counters[idx3] != 0) {
-                System.out.println("There were " + counters[idx3] + " occurences of " + etn.charAt(idx3) + " in string " + (idx3 + 1));
-            }
-        }
+        System.out.println(Arrays.toString(occurences));
     }
 }
