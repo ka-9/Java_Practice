755544315c51ef9d6f4b49f12c7b3c41ffcfba6f
Kevin Aoun <kevin.w.aoun@gmail.com>
2022-05-08 00:23:06 +0300

    Added Bits2HT.java

--- Easy/Bits2HT.java
+++ Easy/Bits2HT.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
 public class Bits2HT {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputStr;
+        System.out.println("Enter a 9 characters string of 0s and 1s: ");
+        inputStr = scanner.nextLine();
+
+        convert2HT(inputStr);
+    }
+    private static void convert2HT(String inputStr) {
+        if (inputStr.length() != 9) {
+            return;
+        }
+        char[] arr = new char[9];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = inputStr.charAt(i);
+        }
+        int counter = 0;
+        for (char bit : arr) {
+            switch(bit) {
+                case '0':
+                System.out.print("H");
+                break;
+                case '1':
+                System.out.print("T");
+            }
+            counter++;
+            if (counter % 3 == 0)
+                System.out.println();
+        }
+    }
 }
