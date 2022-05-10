4bb2511289d1dd4b1b37cc1602281f771232cf07
Kevin Aoun <kevin.w.aoun@gmail.com>
2022-05-10 16:44:57 +0300

    Added BiggestProduct.java

---
+++ Intermediate/BiggestProduct.java
@@ -0,0 +1,15 @@
+package Intermediate;
+public class BiggestProduct extends CountingSortV3{
+
+    //$ The objective is to retur the largest product between two different numbers in an array of ints.
+    //$ The below method findBiggestProduct compares the product of the last 2 ints of the sorted
+    //$ array and deduces the largest product
+    public static void main(String[] args) {
+        int[] arr = {-5, 0, 3, 1, -6, 5, 7, 8, 9, 5, 5, -4, -3};
+        findBiggestProduct(arr);
+    }
+    private static void findBiggestProduct(int[] arr) {
+        int[] arr1 = CountingSortV3.countingSort(arr);
+        System.out.println(Math.max(arr1[0]*arr1[1], arr1[arr1.length-2]*arr1[arr1.length-1]));
+    }  
+}
